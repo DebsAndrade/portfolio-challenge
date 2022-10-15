293829586b84c613e4a1552ba28b0bd5c87f651f
deboraandrade <deboraellenandrade@gmail.com>
2022-10-15 17:46:22 +0100

    Exclua page ONU, será um projeto separado

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,7 +3,6 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import * as S from "../../styles/NavigationStyle";
 import AboutMe from "../../containers/aboutme/AboutMe"
 import Projects from "../../containers/projects/Projects"
-import Onu from "../../containers/onu/Onu"
 
 const Navigation = () => {
     return(
@@ -16,16 +15,12 @@ const Navigation = () => {
                     <S.ListItem>
                         <S.A to="/projects">Projects</S.A>
                     </S.ListItem>
-                    <S.ListItem>
-                        <S.A to="/onu">ONU</S.A>
-                    </S.ListItem>
                 </ul>
             </S.HeaderNavigation>
             
             <Routes>
                 <Route path="/" element={<AboutMe />} />
                 <Route path="/projects" element={<Projects />} />
-                <Route path="/onu" element={<Onu />} />
             </Routes>
         </HashRouter>
     )
--- src/containers/onu/Onu.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Onu = () => {
-    return(
-        <h1>Em construção (ONU)...</h1>
-    )
-}
-
-export default Onu;
